2b6cf068abaaf564891db32fc221cb5ef1a6d9d3
keiki <50743267+999timesgotrejected@users.noreply.github.com>
2020-03-04 08:54:42 +0700

    minor cleanup

--- licences.sql
+++ licences.sql
@@ -1,16 +1,14 @@
 INSERT INTO `licenses` (`type`, `label`) VALUES
 	('dmv', 'Code de la route'),
 	('drive', 'Permis de conduire'),
-		('drive_bike', 'Permis moto'),
+	('drive_bike', 'Permis moto'),
 	('drive_truck', 'Permis camion'),
-		('dmv_taxi', 'Théorie transport rémunérés de personnes'),
+	('dmv_taxi', 'Théorie transport rémunérés de personnes'),
 	('drive_taxi', 'Permis transport rémunérés de personnes'),
-			('dmv_superlourds', 'Théorie poids lourds'),
+	('dmv_superlourds', 'Théorie poids lourds'),
 	('drive_superlourd', 'Permis de conduire poids lourds'),
-				('dmv_superlourds1', 'Théorie superlourds'),
+	('dmv_superlourds1', 'Théorie superlourds'),
 	('drive_superlourd1', 'Permis de conduire Superlourds'),
-		('dmv_chimie', 'Cours de chimie'),
-	('drive_chimie', 'Licence de chimie'),
-	
-
+	('dmv_chimie', 'Cours de chimie'),
+	('drive_chimie', 'Licence de chimie')
 ;
